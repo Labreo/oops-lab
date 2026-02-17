ffb893ae4ce588de3c703f18f7df4294a037d030
Labreo <74888611+Labreo@users.noreply.github.com>
2026-02-17 19:57:04 +0530

    exp3 first 2 questions

---
+++ Exp3/anagramcheck.java
@@ -0,0 +1,41 @@
+package Exp3;
+
+
+import java.util.Scanner;
+
+
+public class anagramcheck {
+    public static boolean anagram(String s1,String s2) {
+        boolean flag = false;
+        for(char c1:s1.toCharArray()){
+         for(char c2:s2.toCharArray()){
+            if(c1==c2){
+             flag=true;
+            }
+         } 
+         if(flag == false){
+            return false;
+         }
+         flag = false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first string:");
+        String s1 = scanner.nextLine();
+        s1=s1.toLowerCase();
+           System.out.println("Enter the second string:");
+        String s2 = scanner.nextLine();  
+        s2=s2.toLowerCase();
+        if(s1.length()!=s2.length()){
+            System.out.println("The strings are not the same length so they cannot be anagrams.");
+        }else{
+            if(anagram(s1, s2)){
+                System.out.println("They are anagrams");
+            }else{
+                System.out.println("They are not anagrams");
+            }
+        }
+    }
+}
---
+++ Exp3/nonrepeatedchar.java
@@ -0,0 +1,28 @@
+package Exp3;
+
+import java.util.Scanner;
+
+public class nonrepeatedchar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str = sc.nextLine().toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println("The first non repeated char is " + str.charAt(i));
+                return;
+            }
+        }
+
+        System.out.println("No non-repeated character found.");
+    }
+}
