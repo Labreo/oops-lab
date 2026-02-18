f90130687b492d30b553e4d8cebc68cad48ad00e
Labreo <74888611+Labreo@users.noreply.github.com>
2026-02-18 16:10:42 +0530

    StringtottitleCase

---
+++ Exp3/StringToTitleCase.java
@@ -0,0 +1,19 @@
+
+package Exp3;
+
+import java.util.*;
+
+public class StringToTitleCase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine().toLowerCase();
+        System.out.println("Enter the string which you want to convert into a titleCase:");
+        String[] parts = s1.split(" ");
+
+        StringBuilder result = new StringBuilder();
+        for(String p:parts){
+            result = result.append(Character.toTitleCase(p.charAt(0))).append(p.substring(1)).append(" ");
+        }
+        System.out.println(result.toString().trim());
+    }
+}
