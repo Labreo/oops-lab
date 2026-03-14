a62e0c11c3ce6e6bfd0b9350c23abf5d4a9fe483
Labreo <74888611+Labreo@users.noreply.github.com>
2026-03-14 15:35:46 +0530

    5.3-fixed

--- Exp5/VotingEligibilityChecker.java
+++ Exp5/VotingEligibilityChecker.java
@@ -13,13 +13,22 @@ class Voter{
     public int getAge(){
         return age;
     }
+    public boolean checkEligibilty(){
+        if(age>=18){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
 class VoterEligibility{
+
+    
 
     public static int countEligible(Voter[] votes){
         int num=0;
         for(Voter vote:votes){
-            if(vote.getAge()>=18){
+            if(vote.checkEligibilty()==true){
               ++num;
             }
         }
@@ -33,15 +42,17 @@ public class VotingEligibilityChecker {
         System.out.println("Enter the No of voters you want to add:\n");
         int num = sc.nextInt();
         Voter[] votes = new Voter[num];
+        sc.nextLine();
         for(int i =0;i<num;++i){
                    System.out.println("Enter the name you want to add:\n");
                    String name = sc.nextLine();
                    System.out.println("Enter the age of the voter:\n");
                    int age = sc.nextInt();
+                   sc.nextLine();
                    votes[i] = new Voter(age, name);
         }
         int evotes = VoterEligibility.countEligible(votes);
-        System.out.println("The of eligible voters are:"+evotes);
+        System.out.println("The no of eligible voters are:"+evotes);
         sc.close();
     }
 }
