c4949fc9184307d9f218c02ec14390b037a4e10c
Labreo <74888611+Labreo@users.noreply.github.com>
2026-03-14 10:51:40 +0530

    5.3

--- Exp5/VotingEligibilityChecker.java
+++ Exp5/VotingEligibilityChecker.java
@@ -1,7 +1,47 @@
 package Exp5;
 
-import java.util.*;
+import java.util.Scanner;
 
+class Voter{
+ public String name;
+    public int age;
+
+    public Voter(int age,String name){
+        this.age=age;
+        this.name = name;
+    }
+    public int getAge(){
+        return age;
+    }
+}
+class VoterEligibility{
+
+    public static int countEligible(Voter[] votes){
+        int num=0;
+        for(Voter vote:votes){
+            if(vote.getAge()>=18){
+              ++num;
+            }
+        }
+       return num;
+    }
+}
 public class VotingEligibilityChecker {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.println("Enter the No of voters you want to add:\n");
+        int num = sc.nextInt();
+        Voter[] votes = new Voter[num];
+        for(int i =0;i<num;++i){
+                   System.out.println("Enter the name you want to add:\n");
+                   String name = sc.nextLine();
+                   System.out.println("Enter the age of the voter:\n");
+                   int age = sc.nextInt();
+                   votes[i] = new Voter(age, name);
+        }
+        int evotes = VoterEligibility.countEligible(votes);
+        System.out.println("The of eligible voters are:"+evotes);
+        sc.close();
+    }
 }
